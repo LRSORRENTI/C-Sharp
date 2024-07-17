e19f437f406431c3b3e1bb7e4aa8b0f8bf11e4d2
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-07-17 09:48:03 -0500

    added basic addition example in C#

--- 01_Intro/06_More_About_Int.cs
+++ 01_Intro/06_More_About_Int.cs
@@ -1,13 +1,28 @@
 // More on the INT datatype and Parsing
 
+// More on the INT datatype and Parsing
+
 Console.WriteLine("Enter a whole number, not text like ten for 10, just enter the number 10");
 
+// Initialize number variables;
 int myNumber = 0;
+int mySecondNumber = 0;
 
+// Grab the first user input number
 string userInput = Console.ReadLine()!;
 
-myNumber = int.Parse(userInput);
+// Ask for second user input number 
+Console.WriteLine("Please enter a second number to sum with the first");
 
-System.Console.WriteLine("My number is " + myNumber);
+// Grab second user input number 
+string secondInput = Console.ReadLine()!;
+
+// Turn the string input for both into actual int data type 
+// using int.Parse for each 
+myNumber = int.Parse(userInput);
+mySecondNumber = int.Parse(secondInput);
+
+// Sum the two inputs and return them to the user 
+System.Console.WriteLine("The sum is " + (myNumber + mySecondNumber));
 
 Console.ReadKey();
